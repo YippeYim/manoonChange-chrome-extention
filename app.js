32719304320eaae2c2d5e5c2deac4add7bd14544
Piya Yim <piya.yim2003@gmail.com>
2022-12-03 21:25:17 +0700

    better look, darkmode switching

--- app.js
+++ app.js
@@ -27,3 +27,33 @@ function copy(){
     textarea.select()
     navigator.clipboard.writeText(textarea.value)
 }
+
+// edit theme color
+let isDarkMode = false
+let bgcolor,
+    darkercolor,
+    lightercolor,
+    fontcolor
+
+const btn = document.getElementById('switch')
+
+btn.addEventListener("click",()=>{
+    if(isDarkMode){
+        bgcolor="rgb(244, 244, 244)"
+        darkercolor="rgb(203, 203, 203)"
+        lightercolor="rgb(248, 248, 248)"
+        fontcolor="rgb(0,0,0)"
+    }
+    if(!isDarkMode){
+        bgcolor="#212121"
+        darkercolor="#2E2E2E"
+        lightercolor="#717171"
+        fontcolor="rgb(255,255,255)"
+    }
+
+    document.documentElement.style.setProperty('--bgcolor',bgcolor)
+    document.documentElement.style.setProperty('--darkercolor',darkercolor)
+    document.documentElement.style.setProperty('--lightercolor',lightercolor)
+    document.documentElement.style.setProperty('--fontcolor',fontcolor)
+    isDarkMode = !isDarkMode
+})
